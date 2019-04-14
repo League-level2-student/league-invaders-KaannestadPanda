807b4b7168b5495d6678c12ea279dceefd626e32
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-04-14 12:57:08 -0700

    leagueinvaderssss

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
+JFrame frame;
+final int width=500;
+final int height=800;
 
+	public static void main(String[] args) {
+	
+	
+	}
+	
+	LeagueInvaders(){
+		frame=new JFrame();
+		
+	}
+	
+	void setup() {
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		
+	}
+	
+	
 }
