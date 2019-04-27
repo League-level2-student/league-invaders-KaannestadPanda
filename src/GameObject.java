debd90877222a09f31e713ee5b5b21daa6352a47
KaannestadPanda <league@WTS14-2.attlocal.net>
2019-04-27 12:58:41 -0700

    gameObject, rocketship

--- src/GameObject.java
+++ src/GameObject.java
@@ -15,14 +15,11 @@ GameObject(int newX, int newY, int newWidth, int newHeight){
 }
 
 void update() {
-	Random widthran = new Random();
-	x=widthran.nextInt(LeagueInvaders.width-99);
-	Random heightran = new Random();
-	y=heightran.nextInt(LeagueInvaders.height-99);
+	
 }
 
 void draw(Graphics ge) {
-	ge.fillRect(x,y,100,100);
+	
 }
 
 	
--- src/Rocketship.java
+++ src/Rocketship.java
@@ -1,4 +1,24 @@
+import java.awt.Graphics;
 
-public class Rocketship {
+public class Rocketship extends GameObject{
 
+	Rocketship(int newX, int newY, int newWidth, int newHeight) {
+		super(newX, newY, newWidth, newHeight);
+		// TODO Auto-generated constructor stub
+	}
+
+void update() {
+	
+}
+
+void draw(Graphics eg) {
+	
+}
+	
+	
+	
+	
+	
+	
+	
 }
